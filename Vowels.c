1f38866f16b2f9278d93e528628c2589495a7ddc
Somesh Dattatraya Dahotre <131247687+totally-somesh@users.noreply.github.com>
2023-04-30 18:26:05 +0530

    Vowels Recognition
    ~ Vowels are an important part of English Grammar.
    ~ Here, vowels are detected by using a C program. 
    ~ Check it out.

---
+++ Vowels.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void main()
+{
+    char ch;
+    
+    printf("Enter A Character.\n");
+    scanf("%c",&ch);
+    
+    if(ch == 'a')
+    {
+        printf("Entered Charcter : %c Is A Vowel.",ch);
+    }
+    else if(ch == 'e')
+    {
+        printf("Entered Charcter : %c Is A Vowel.",ch);
+    }
+    else if(ch == 'i')
+    {
+        printf("Entered Charcter : %c Is A Vowel.",ch);
+    }
+    else if(ch == 'o')
+    {
+        printf("Entered Charcter : %c Is A Vowel.",ch);
+    }
+    else if(ch == 'u')
+    {
+        printf("Entered Charcter : %c Is A Vowel.",ch);
+    }
+    else
+    {
+        printf("Entered Charcter : %c Is Not A Vowel.",ch);
+    }
+    
+}
+   
+
+
+
